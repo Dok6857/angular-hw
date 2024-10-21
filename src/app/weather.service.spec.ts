b7e32c21ad29870f660f7cca9e699c72c73dcab7
Dok6857 <Dok6857@gmail.com>
2024-10-21 18:31:31 +0300

    add tests

--- src/app/weather.service.spec.ts
+++ src/app/weather.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 describe('WeatherService', () => {
   let service: WeatherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpTestingController],
+      providers: [WeatherService],
+    });
     service = TestBed.inject(WeatherService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should fetch weather data', () => {
+    const testWeather = {
+      name: 'Sumy',
+      main: { temp: 15 },
+      weather: [{ main: 'Clouds' }],
+    };
+
+    service.getWeather('Sumy').subscribe((data) => {
+      expect(data.city).toBe('Sumy');
+      expect(data.temperature).toBe(15);
+    });
+
+    const req = httpMock.expectOne(
+      `${service['API_URL']}?q=Kyiv&units=metric&appid=${service['API_KEY']}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(testWeather);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 });
